513173bcb0e2399e56825f8d0192b9ac4f9499f9
Fairday <aleksey1565@mail.ru>
2020-08-22 19:40:27 +0300

    TIQF (1-2)

---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/TopInverviewQuestions_Free/1_RemoveDuplicatesFromSortedArray_26.cs
@@ -0,0 +1,69 @@
+﻿namespace ALGON.LeetCodeProblems.TopInverviewQuestions_Free
+{
+    //Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+    //Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+    //Example 1:
+
+    //Given nums = [1,1,2],
+
+    //Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+    //It doesn't matter what you leave beyond the returned length.
+    //Example 2:
+
+    //Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+    //Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+    //It doesn't matter what values are set beyond the returned length.
+    //Clarification:
+
+    //Confused why the returned value is an integer but your answer is an array?
+
+    //Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+    //Internally you can think of this:
+
+    //// nums is passed in by reference. (i.e., without making a copy)
+    //int len = removeDuplicates(nums);
+
+    //// any modification to nums in your function would be known by the caller.
+    //// using the length returned by your function, it prints the first len elements.
+    //for (int i = 0; i < len; i++) {
+    //        print(nums[i]);
+    //    }
+    public class Solution_26
+    {
+        //Time complexity: O(nums.Length)
+        //Space complexity: O(1)
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            //We need to collect unique integers from the start of nums (in place)
+            //Use two pointer technique
+            //Example: 0011123
+            //j = 0; (second pointer)
+            //0011123 j = 0 i = 0
+            //0011123 j = 0 i = 1
+            //0111123 j = 1 i = 2
+            //0111123 j = 1 i = 3
+            //0111123 j = 1 i = 4
+            //0121123 j = 2 i = 5
+            //0123123 j = 3 i = 6
+            //[0123 - unique elements, answer j + 1][123]
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    j++;
+                    nums[j] = nums[i];
+                }
+            }
+            return j + 1;
+        }
+    }
+}
---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/TopInverviewQuestions_Free/2_BestTimeToBuyAndSellStock2_122.cs
@@ -0,0 +1,53 @@
+﻿namespace ALGON.LeetCodeProblems.TopInverviewQuestions_Free
+{
+    //Say you have an array prices for which the ith element is the price of a given stock on day i.
+    //Design an algorithm to find the maximum profit.You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+    //Note: You may not engage in multiple transactions at the same time(i.e., you must sell the stock before you buy again).
+
+    //Example 1:
+
+    //Input: [7,1,5,3,6,4]
+    //    Output: 7
+    //Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+    //             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+    //Example 2:
+
+    //Input: [1,2,3,4,5]
+    //    Output: 4
+    //Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+    //             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+    //             engaging multiple transactions at the same time.You must sell before buying again.
+    //Example 3:
+
+    //Input: [7,6,4,3,1]
+    //Output: 0
+    //Explanation: In this case, no transaction is done, i.e.max profit = 0.
+
+    //Constraints:
+
+    //1 <= prices.length <= 3 * 10 ^ 4
+    //0 <= prices[i] <= 10 ^ 4
+    public class Solution_122
+    {
+        //Time complexity: O(prices.Length)
+        //Space complexity: O(1)
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 0) return 0;
+
+            //Solution: we need to buy stock only if we will have positive profit (prices[i] - prices[i - 1])
+            //Example: 1 7 2 6
+            //Buy on 1, because 7 - 1 > 0: +6
+            //Buy on 2, becauyse 6 - 2 > 0: +4
+            //Answer = 10
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+            }
+
+            return maxProfit;    
+        }
+    }
+}
