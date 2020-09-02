c57774aee9c9264a60def271bc300ffc737b80d2
Fairday <aleksey1565@mail.ru>
2020-09-02 22:45:52 +0300

    sortedset problem

---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/Hashtable/ContainsDuplicate3_220.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ALGON.LeetCodeProblems.Hashtable
+{
+    //Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+
+    //Example 1:
+
+    //Input: nums = [1,2,3,1], k = 3, t = 0
+    //Output: true
+    //Example 2:
+
+    //Input: nums = [1,0,1,1], k = 1, t = 2
+    //Output: true
+    //Example 3:
+
+    //Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+    //Output: false
+    public class Solution_220
+    {
+        //TC: O(n^2)
+        //SC: O(1)
+        public bool ContainsNearbyAlmostDuplicate1(int[] nums, int k, int t)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs((double)nums[i] - (double)nums[j]) <= (double)t && Math.Abs(i - j) <= k)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        //TC: O(n logn)
+        //SC: O(n)
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            if (k < 0 || t < 0)
+                return false;
+
+            var sortedSet = new SortedSet<long>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sortedSet.GetViewBetween((long)nums[i] - t, (long)nums[i] + t).Any())
+                    return true;
+
+                sortedSet.Add(nums[i]);
+
+                if (i >= k)
+                    sortedSet.Remove(nums[i - k]);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/Math/LargestTimeForGivenDigits_949.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ALGON.LeetCodeProblems.PMath
+{
+    //Given an array of 4 digits, return the largest 24 hour time that can be made.
+
+    //The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.
+
+    //Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
+
+    //Example 1:
+
+    //Input: [1,2,3,4]
+    //Output: "23:41"
+    //Example 2:
+
+    //Input: [5,5,5,5]
+    //Output: ""
+
+    //Note:
+
+    //A.length == 4
+    //0 <= A[i] <= 9
+    public class Solution_949
+    {
+        int maxTime = -1;
+
+        //TC^ O(n!), but n is constant that equal 4, so TC -> O(1), SC -> O(1)
+        public string LargestTimeFromDigits(int[] A)
+        {
+            Permute(A, 0);
+            if (maxTime == -1)
+                return string.Empty;
+            else
+                return TimeSpan.FromHours(maxTime / 60).ToString("hh") + ":" + TimeSpan.FromMinutes(maxTime % 60).ToString("mm");
+        }
+
+        void Permute(int[] A, int start)
+        {
+            if (A.Length == start)
+            {
+                BuildTime(A);
+            }
+
+            for (int i = start; i < A.Length; i++)
+            {
+                Swap(A, start, i);
+                Permute(A, start + 1);
+                Swap(A, start, i);
+            }
+        }
+
+        void BuildTime(int[] pm)
+        {
+            var hour = pm[0] * 10 + pm[1];
+            var min = pm[2] * 10 + pm[3];
+            if (hour < 24 && min < 60)
+                maxTime = Math.Max(maxTime, hour * 60 + min);
+        }
+
+        void Swap(int[] arr, int i, int j)
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
