65cb081cb53512245f4c9d78e5dc19f4af8b5081
Fairday <aleksey1565@mail.ru>
2020-09-20 16:46:03 +0300

    tiq problems

---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/TopInverviewQuestions_Free/7_PlusOne_66.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ALGON.LeetCodeProblems.TopInverviewQuestions_Free
+{
+    //Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+    //The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+    //You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+    //Example 1:
+
+    //Input: digits = [1, 2, 3]
+    //Output: [1,2,4]
+    //Explanation: The array represents the integer 123.
+    //Example 2:
+
+    //Input: digits = [4, 3, 2, 1]
+    //Output: [4,3,2,2]
+    //Explanation: The array represents the integer 4321.
+    //Example 3:
+
+    //Input: digits = [0]
+    //Output: [1]
+
+
+    //Constraints:
+
+    //1 <= digits.length <= 100
+    //0 <= digits[i] <= 9
+    public class Solution_66
+    {
+        //TC: O(N)
+        //SC: O(N)
+        public int[] PlusOne(int[] digits)
+        {
+            var l = digits.Length;
+
+            var remaining = 0;
+            digits[l - 1]++;
+            for (int i = l - 1; i >= 0; i--)
+            {
+                var num = digits[i] + remaining;
+                if (num > 9)
+                {
+                    remaining = 1;
+                    num -= 10;
+                }
+                else
+                    remaining = 0;
+
+                digits[i] = num;
+            }
+
+            if (remaining == 1)
+            {
+                int[] newArray = new int[l + 1];
+                newArray[0] = 1;
+                Array.Copy(digits, 0, newArray, 1, l);
+                return newArray;
+            }
+
+            return digits;
+        }
+    }
+}
---
+++ ALGON.LeetCodeProblems/ALGON.LeetCodeProblems/TopInverviewQuestions_Free/8_MoveZeroes_283.cs
@@ -0,0 +1,37 @@
+﻿namespace ALGON.LeetCodeProblems.TopInverviewQuestions_Free
+{
+    //Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+    //Example:
+
+    //Input: [0,1,0,3,12]
+    //    Output: [1,3,12,0,0]
+    //    Note:
+
+    //You must do this in-place without making a copy of the array.
+    //Minimize the total number of operations.
+    public class Solution_283
+    {
+        //TC: O(N)
+        //SC: O(1)
+        public void MoveZeroes(int[] nums)
+        {
+            if (nums != null)
+            {
+                var j = -1;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    var num = nums[i];
+                    if (num != 0 && j >= 0)
+                    {
+                        nums[j] = num;
+                        nums[i] = 0;
+                        j++;
+                    }
+                    else if (j < 0 && num == 0)
+                        j = i;
+                }
+            }
+        }
+    }
+}
