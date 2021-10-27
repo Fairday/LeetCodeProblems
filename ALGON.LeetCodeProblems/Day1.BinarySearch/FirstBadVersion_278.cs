952c078c0a9abaebca864809a65ce17784766306
Fairday <aleksey1565@mail.ru>
2021-10-27 08:25:52 +0300

    278 and 35

---
+++ ALGON.LeetCodeProblems/Day1.BinarySearch/FirstBadVersion_278.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Day1.BinarySearch
+{
+    //You are a product manager and currently leading a team to develop a new product.Unfortunately, the latest version of your product fails the quality check.Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+    //Suppose you have n versions[1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+    //You are given an API bool isBadVersion(version) which returns whether version is bad.Implement a function to find the first bad version.You should minimize the number of calls to the API.
+
+    //Example 1:
+
+    //Input: n = 5, bad = 4
+    //Output: 4
+    //Explanation:
+    //call isBadVersion(3) -> false
+    //call isBadVersion(5) -> true
+    //call isBadVersion(4) -> true
+    //Then 4 is the first bad version.
+
+    //Example 2:
+
+    //Input: n = 1, bad = 1
+    //Output: 1
+
+    //Constraints:
+
+    //1 <= bad <= n <= 231 - 1
+    public class FirstBadVersion_278
+    {
+        //TC: O(N)
+        //SC: O(1)
+        public int FirstBadVersion_NaiveApproach(int n)
+        {
+            if (n <= 0)
+                throw new Exception("n should be more than zero");
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsBadVersion(i))
+                    return i;
+            }
+
+            throw new Exception("bad version should exist");
+        }
+
+        //TC: O(log N)
+        //SC: O(1)
+        public int FirstBadVersion(int n)
+        {
+            if (n <= 0)
+                throw new Exception("n should be more than zero");
+
+            int lo = 1;
+            int hi = n;
+            //int mid = 0;
+
+            while (/*lo <= hi*/ lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+
+                //example
+                // 1 2 3 4 5 6(*) 7 8
+                // 4 5 6(*) 7 8
+                // 4 5 6(*)
+                // 5 6(*)
+                //lo = 5, hi = 6, mid = 5 -> lo = 6
+                //lo = 6, hi = 6, mid = 6
+                //moving in left direction
+                //if (mid == hi)
+                //return mid;
+
+                if (IsBadVersion(mid))
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+
+            //another approarch,  moving lo to hi
+            return lo;
+        }
+
+        private bool IsBadVersion(int version) 
+        {
+            throw new NotImplementedException("LeedCode impl should be here");
+        }
+    }
+}
---
+++ ALGON.LeetCodeProblems/Day1.BinarySearch/SearchInsertPosition_35.cs
@@ -0,0 +1,84 @@
+﻿namespace Day1.BinarySearch
+{
+    //Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+
+    //You must write an algorithm with O(log n) runtime complexity.
+
+    //Example 1:
+
+    //Input: nums = [1, 3, 5, 6], target = 5
+    //Output: 2
+    //Example 2:
+
+    //Input: nums = [1, 3, 5, 6], target = 2
+    //Output: 1
+    //Example 3:
+
+    //Input: nums = [1, 3, 5, 6], target = 7
+    //Output: 4
+    //Example 4:
+
+    //Input: nums = [1, 3, 5, 6], target = 0
+    //Output: 0
+    //Example 5:
+
+    //Input: nums = [1], target = 0
+    //Output: 0
+    public class SearchInsertPosition_35
+    {
+        //TC: O(log N)
+        //SC: O(1)
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int lo = 0;
+            int hi = nums.Length - 1;
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                int num = nums[mid];
+                if (num == target)
+                    return mid;
+                else if (target > num)
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+
+            return lo;
+        }
+
+        //TC: O(N)
+        //SC: O(1)
+        public int SearchInsert_NaiveApproach(int[] nums, int target)
+        {
+            if (nums.Length == 0 || nums[0] > target)
+                return 0;
+
+            if (nums.Length == 1 && nums[0] == target)
+                return 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                //1 3 5
+                //3 -> 2
+                //5 -> 3
+                //4 -> 2
+
+                //1 3
+                //1
+                if (target > nums[i] && target <= nums[i + 1])
+                    return i + 1;
+                else if (target >= nums[i] && target < nums[i + 1])
+                    return i;
+                else if (target >= nums[i] && target <= nums[i + 1])
+                    return i;
+            }
+
+            return nums.Length;
+        }
+    }
+}
