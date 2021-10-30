cde8938efafef1173fc507db616edb5abd16ff43
Fairday <aleksey1565@mail.ru>
2021-10-30 17:42:26 +0300

    344, 557

---
+++ ALGON.LeetCodeProblems/Day4.TwoPointers/ReverseString_344.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Day4.TwoPointers
+{
+    //Write a function that reverses a string. The input string is given as an array of characters s.
+
+    //Example 1:
+
+    //Input: s = ["h","e","l","l","o"]
+    //Output: ["o","l","l","e","h"]
+    //Example 2:
+
+    //Input: s = ["H","a","n","n","a","h"]
+    //Output: ["h","a","n","n","a","H"]
+ 
+    //Constraints:
+
+    //1 <= s.length <= 105
+    //s[i] is a printable ascii character.
+ 
+    //Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+    public class ReverseString_344
+    {
+        //TC: O(N)
+        //SC: O(1)
+        public void ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                Swap(s, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        private void Swap(char[] s, int i, int j)
+        {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+}
---
+++ ALGON.LeetCodeProblems/Day4.TwoPointers/ReverseWordsInAString3_557.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Day4.TwoPointers
+{
+    //Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+    //Example 1:
+
+    //Input: s = "Let's take LeetCode contest"
+    //Output: "s'teL ekat edoCteeL tsetnoc"
+    //Example 2:
+
+    //Input: s = "God Ding"
+    //Output: "doG gniD"
+ 
+    //Constraints:
+
+    //1 <= s.length <= 5 * 104
+    //s contains printable ASCII characters.
+    //s does not contain any leading or trailing spaces.
+    //There is at least one word in s.
+    //All the words in s are separated by a single space.
+    public class ReverseWordsInAString3_557
+    {  
+        //TC: O(N)
+        //SC: O(N)
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+
+            string[] parts = s.Split(' ');
+            StringBuilder res = new StringBuilder(s.Length);
+            foreach (string part in parts)
+            {
+                res.Append(Reverse(part));
+                res.Append(" ");
+            }
+
+            res.Remove(s.Length, 1);
+            return res.ToString();
+
+        }
+
+        private string Reverse(string s)
+        {
+            StringBuilder sb = new StringBuilder(s.Length);
+            for (int i = s.Length - 1; i >= 0; i--)
+                sb.Append(s[i]);
+            return sb.ToString();
+        }
+    }
+}
