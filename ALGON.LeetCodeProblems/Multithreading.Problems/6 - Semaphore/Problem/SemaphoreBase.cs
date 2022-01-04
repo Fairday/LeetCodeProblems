e860464385ce5f6ff8f82a630fcfa71bf71f9672
Fairday <aleksey1565@mail.ru>
2022-01-04 18:28:05 +0300

    semaphore implementation

---
+++ ALGON.LeetCodeProblems/Multithreading.Problems/6 - Semaphore/Problem/SemaphoreBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Multithreading.Problems._6___Semaphore.Problem
+{
+    public abstract class SemaphoreBase
+    {
+        protected int _availablePermits;
+        protected int _maxPermits;
+
+        protected SemaphoreBase(int availablePermits, int maxPermits)
+        {
+            if (availablePermits < 0)
+                throw new ArgumentOutOfRangeException("availablePermits should be more or equal than zero");
+
+            if (maxPermits <= 0)
+                throw new ArgumentOutOfRangeException("maxPermits should be more than zero");
+
+            if (availablePermits > maxPermits)
+                throw new ArgumentOutOfRangeException("availablePermits should be less or equal than maxPermits");
+
+            _availablePermits = availablePermits;
+            _maxPermits = maxPermits;
+        }
+
+        public abstract void Acquire();
+        public abstract void Release();
+    }
+}
---
+++ ALGON.LeetCodeProblems/Multithreading.Problems/6 - Semaphore/Solutions/MySemaphore.cs
@@ -0,0 +1,53 @@
+﻿using Multithreading.Problems._6___Semaphore.Problem;
+using System.Threading;
+
+namespace Multithreading.Problems._6___Semaphore.Solutions
+{
+    public sealed class MySemaphore : SemaphoreBase
+    {
+        private readonly object _padLock;
+
+        public MySemaphore(int availablePermits, int maxPermits) : base(availablePermits, maxPermits)
+        {
+            _padLock = new object();
+        }
+
+        public override void Acquire()
+        {
+            //try to enter in critical section
+            Monitor.Enter(_padLock);
+
+            //no available permits
+            while (_availablePermits == 0)
+                //wait for permit
+                Monitor.Wait(_padLock);
+
+            //gain a permit
+            _availablePermits--;
+
+            //notify all threads in wait set
+            Monitor.PulseAll(_padLock);
+            //exit from critical section
+            Monitor.Exit(_padLock);
+        }
+
+        public override void Release()
+        {
+            //try to enter in critical section
+            Monitor.Enter(_padLock);
+
+            //all permits available
+            while (_availablePermits == _maxPermits)
+                //wait for possibility to return permit
+                Monitor.Wait(_padLock);
+
+            //return a permit
+            _availablePermits++;
+
+            //notify all threads in wait set
+            Monitor.PulseAll(_padLock);
+            //exit from critical section
+            Monitor.Exit(_padLock);
+        }
+    }
+}
